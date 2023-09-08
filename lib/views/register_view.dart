017cc1731b10f9aae8a8b3b3fdf1e0999e6c8f9f
Adhish Shakya <shakyaadhish@gmail.com>
2023-09-08 21:50:25 +0545

    error handling using alert dialog

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'dart:developer' as devtools show log;
 import 'package:notes_app/constants/routes.dart';
+import 'package:notes_app/utilities/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -85,23 +85,27 @@ class _RegisterViewState extends State<RegisterView> {
                   final email = _email.text;
                   final password = _password.text;
                   try {
-                    final userCredential = await FirebaseAuth.instance
-                        .createUserWithEmailAndPassword(
-                            email: email, password: password);
-                    devtools.log(userCredential.toString());
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    final user = FirebaseAuth.instance.currentUser;
+                    await user?.sendEmailVerification();
                     if (context.mounted) {
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                          loginRoute, (route) => false);
+                      Navigator.of(context).pushNamed(verifyEmailRoute);
                     }
                   } on FirebaseAuthException catch (e) {
                     if (e.code == "weak-password") {
-                      devtools
-                          .log("Password must contain at least 6 characters");
+                      await showErrorDialog(context, "Weak Password");
                     } else if (e.code == "email-already-in-use") {
-                      devtools.log("Email already in use");
+                      showErrorDialog(context, "Email already in use");
                     } else if (e.code == "invalid-email") {
-                      devtools.log("Invalid email address");
+                      showErrorDialog(context, "Invalid Email Address");
+                    } else {
+                      showErrorDialog(context, "Error - ${e.code}");
                     }
+                  } catch (e) {
+                    showErrorDialog(context, e.toString());
                   }
                 },
                 child: const Text("Register")),
