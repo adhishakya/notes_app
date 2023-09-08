866c8fd96e798508034950189fc8f12b1b46e513
Adhish Shakya <shakyaadhish@gmail.com>
2023-09-08 20:10:24 +0545

    option to logout while verifying

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import '../constants/routes.dart';
 import '../firebase_options.dart';
+import '../main.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -17,7 +19,33 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       options: DefaultFirebaseOptions.currentPlatform,
     );
     return Scaffold(
-      appBar: AppBar(title: const Text("Verify email")),
+      appBar: AppBar(
+        title: const Text("Verify email"),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    if (context.mounted) {
+                      Navigator.of(context)
+                          .pushNamedAndRemoveUntil(loginRoute, (_) => false);
+                    }
+                  }
+                  break;
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                    value: MenuAction.logout, child: Text('Logout')),
+              ];
+            },
+          )
+        ],
+      ),
       body: Column(
         children: [
           const Text("Please verify your email address"),
@@ -32,4 +60,30 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       ),
     );
   }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text("Logout?"),
+        content: const Text("Are you sure you want to log out?"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text("Confirm"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text("Cancel"),
+          ),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
 }
