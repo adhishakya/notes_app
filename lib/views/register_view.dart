a5e6348c7ecbe9c88e793d9e07de27e4b06a3589
Adhish Shakya <shakyaadhish@gmail.com>
2023-09-08 19:42:06 +0545

    logging processess

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -73,14 +74,15 @@ class _RegisterViewState extends State<RegisterView> {
                     final userCredential = await FirebaseAuth.instance
                         .createUserWithEmailAndPassword(
                             email: email, password: password);
-                    print(userCredential);
+                    devtools.log(userCredential.toString());
                   } on FirebaseAuthException catch (e) {
                     if (e.code == "weak-password") {
-                      print("Password must contain atleast 6 characters");
+                      devtools
+                          .log("Password must contain at least 6 characters");
                     } else if (e.code == "email-already-in-use") {
-                      print("Email already in use");
+                      devtools.log("Email already in use");
                     } else if (e.code == "invalid-email") {
-                      print("Invalid email address");
+                      devtools.log("Invalid email address");
                     }
                   }
                 },
