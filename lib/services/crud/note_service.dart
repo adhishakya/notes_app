83cc9a8664e0ee60440d2bacea286d774cf5e53e
Adhish Shakya <shakyaadhish@gmail.com>
2023-09-18 20:35:58 +0545

    moving constants for crud

--- lib/services/crud/note_service.dart
+++ lib/services/crud/note_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:notes_app/extensions/list/filter.dart';
+import 'package:notes_app/services/crud/crud_constants.dart';
 import 'package:notes_app/services/crud/crud_exceptions.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
@@ -320,27 +321,3 @@ class DatabaseNote {
   @override
   int get hashCode => id.hashCode;
 }
-
-const dbName = "notes.db";
-const notesTable = "note";
-const userTable = "user";
-const idColumn = "id";
-const emailColumn = "email";
-const userIdColumn = "user_id";
-const titleColumn = "title";
-const textColumn = "text";
-const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
-        "id"	INTEGER NOT NULL,
-        "email"	TEXT NOT NULL UNIQUE,
-        PRIMARY KEY("id" AUTOINCREMENT)
-      );
-      ''';
-const createNoteTable = '''CREATE TABLE IF NOT EXISTS "note" (
-        "id"	INTEGER NOT NULL,
-        "user_id"	INTEGER NOT NULL,
-        "title"	TEXT NOT NULL,
-        "text"	TEXT,
-        FOREIGN KEY("user_id") REFERENCES "user"("id"),
-        PRIMARY KEY("id" AUTOINCREMENT)
-      );
-      ''';
