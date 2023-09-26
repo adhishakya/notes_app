e534deab1f2027341f153779f56c6542c3078052
Adhish Shakya <shakyaadhish@gmail.com>
2023-09-26 20:23:21 +0545

    added forgot password route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants/routes.dart';
 import 'package:notes_app/services/auth/auth_service.dart';
+import 'package:notes_app/views/forgot_password_view.dart';
 import 'package:notes_app/views/login_view.dart';
 import 'package:notes_app/views/notes/create_update_note_view.dart';
 import 'package:notes_app/views/notes/notes_view.dart';
@@ -19,6 +20,7 @@ void main() {
       notesRoute: (context) => const NotesView(),
       verifyEmailRoute: (context) => const VerifyEmailView(),
       createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
+      forgotPasswordRoute: (context) => const ForgotPasswordView(),
     },
   ));
 }
